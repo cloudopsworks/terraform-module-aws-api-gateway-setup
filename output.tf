26fcc6290a9908a9e3579608e4e45720cee2eaec
Cristian Beraha <cristian@cloudops.works>
2024-10-01 16:56:10 -0300

    fix: comprehensive output

--- output.tf
+++ output.tf
@@ -6,13 +6,13 @@
 
 output "apigw_domains" {
   value = {
-    for item in var.apigw_domains : item => {
-      arn                    = aws_api_gateway_domain_name.this[item].arn
-      id                     = aws_api_gateway_domain_name.this[item].id
-      cloudfront_domain_name = aws_api_gateway_domain_name.this[item].cloudfront_domain_name
-      cloudfront_zone_id     = aws_api_gateway_domain_name.this[item].cloudfront_zone_id
-      regional_domain_name   = aws_api_gateway_domain_name.this[item].regional_domain_name
-      regional_zone_id       = aws_api_gateway_domain_name.this[item].regional_zone_id
+    for item in aws_api_gateway_domain_name.this : item.domain_name => {
+      arn                    = item.arn
+      id                     = item.id
+      cloudfront_domain_name = item.cloudfront_domain_name
+      cloudfront_zone_id     = item.cloudfront_zone_id
+      regional_domain_name   = item.regional_domain_name
+      regional_zone_id       = item.regional_zone_id
     }
   }
 }
